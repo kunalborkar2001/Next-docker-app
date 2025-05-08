c6270f8f22063c674ea24cce467815229a1c1327
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2025-05-08 14:13:35 +0530

    updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ async function Home() {
     return (
       <main className="p-8 bg-gray-100 min-h-screen">
         <section className="max-w-7xl mx-auto">
-          <h1 className="text-4xl font-bold mb-6">Registered Tasks 1</h1>
+          <h1 className="text-4xl font-bold mb-6">Registered Tasks 2</h1>
           <div className="text-gray-600 text-left w-full">
             <p>No tasks found.</p>
           </div>
